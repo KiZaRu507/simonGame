37f4c65ca046de868301547c32a32a351ef2ecb1
Sumukh VR <vrsumukh32@gmail.com>
2021-09-22 18:54:50 +0530

    html and css completed js pending

---
+++ script.js
@@ -0,0 +1,19 @@
+const box1=document.querySelector('.box1');
+const box2=document.querySelector('.box2');
+const box3=document.querySelector('.r2box1');
+const box4=document.querySelector('.r2box2');
+box4.addEventListener('click',()=>{
+    console.log('clicked');
+})
+box1.addEventListener('click',()=>{
+    console.log('clicked');
+})
+box2.addEventListener('click',()=>{
+    console.log('clicked');
+})
+box3.addEventListener('click',()=>{
+    console.log('clicked');
+})
+function playGame(){
+    
+}
